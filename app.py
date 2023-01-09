5bd7681be349bb58cb5ef80fa7dd7b19e288a3eb
andy0427s <andy0427s@gmail.com>
2023-01-09 02:42:04 +0000

    Create for deployment test

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
 from blog import app as application
+
 app = application
 
 if __name__ == '__main__':
-    app.run()
+    app.run(
+        host='0.0.0.0',
+        port=8080,
+        debug=True
+    )
